7b4fbd6a05e231f4036ba54dc0e74a272498f9f4
lulungsatrioprayuda <alungsatrio12@gmail.com>
2022-07-18 03:29:01 +0800

    progress
    add table make migration update the setting controller

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Setting;
+
 class SettingController extends Controller
 {
     function index()
@@ -12,5 +14,17 @@ class SettingController extends Controller
             'title' => 'Post',
             'meta_desc' => 'Post Page',
         ]);
+    }
+
+    function save_setting(Request $request)
+    {
+        $data = new Setting;
+        $data->comment_auto = $request->comment_auto;
+        $data->user_auto = $request->user_auto;
+        $data->recent_limit = $request->recent_limit;
+        $data->popular_limit = $request->popular_limit;
+        $data->recent_comment_limit = $request->recent_comment_limit;
+        $data->save();
+        return redirect('admin/setting')->with('success', 'Data success to edit');
     }
 }
