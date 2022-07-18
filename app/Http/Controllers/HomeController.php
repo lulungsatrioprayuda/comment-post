0dccd4534e98ea7ba1d516f5ce2952777874a812
lulungsatrioprayuda <alungsatrio12@gmail.com>
2022-07-19 02:34:44 +0800

    create ui post
    done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
     function home()
     {
-        return view('home');
+        $posts = Post::orderBy('id', 'desc')->simplePaginate(1);
+        return view(
+            'home',
+            [
+                'posts' => $posts,
+            ]
+        );
     }
 }
