94d26eea1cc00eb8ee13f37929fb05cf0b98101f
lulungsatrioprayuda <alungsatrio12@gmail.com>
2022-07-19 23:33:38 +0800

    Update detail.blade.php

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,15 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('frontlayout')
+@section('title','Home Page')
+@section('content')
+<div class="row">
+    <div class="col-md 8">
+        <div class="card">
+            <h5 class="card-header">{{$detail->title}}</h5>
+            <img src="{{asset('imgs/full/'. $detail->full_img)}}" alt="{{$detail->detail}}" class="card-img-top">
+            <div class="card-body">
+                {{$detail->detail}}
+            </div>
+        </div>
+    </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    <h3>Detail page</h3>
-</body>
-
-</html>
+    <div class="col-md-4">
+        <div class="card mb-4">
+            <h5 class="card-header">Search</h5>
+            <div class="card-body">
+                <form action="{{url('/')}}">
+                    <div class="input-group">
+                        <input type="text" name="q" class="form-control" />
+                        <div class="input-group-append">
+                            <button class="btn btn-dark" type="button" id="button-addon2">Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <h5 class="card-header">Recent Posts</h5>
+            <div class="list-group list-group-flush">
+                @if($recent_posts)
+                @foreach($recent_posts as $post)
+                <a href="#" class="list-group-item">{{$post->title}}</a>
+                @endforeach
+                @endif
+            </div>
+        </div>
+        <div class="card mb-4">
+            <h5 class="card-header">Popular Posts</h5>
+            <div class="list-group list-group-flush">
+                <a href="#" class="list-group-item">Post 1</a>
+                <a href="#" class="list-group-item">Post 2</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
